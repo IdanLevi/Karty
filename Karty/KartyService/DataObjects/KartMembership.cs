e9a65b9f07fabb3ef1f9e087f4e935372af7f026
idanlevi6 <idanlevi6@gmail.com>
2017-05-25 01:43:10 +0300

    Added the service project and updated some packages + Some Models. Do Not Run Service yet

---
+++ Karty/KartyService/DataObjects/KartMembership.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KartyService.DataObjects
+{
+    public class KartMembership : EntityData
+    {
+        public string UserId { get; set; }
+
+        public string KartId { get; set; }
+    }
+}
---
+++ Karty/KartyService/DataObjects/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KartyService.DataObjects
+{
+    public class User : EntityData
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
